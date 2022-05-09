4ba6f75f319c83448238c6a0b9a19be95265fc99
Juan Campos Barezzi <juan.barezzi@gmail.com>
2022-05-09 17:59:14 -0300

    Update User.js

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,7 +1,7 @@
 export default function User(props) {
     return(
         <div class="user">
-            <img src={`assets/img/${props.image}`} alt={`Foto de perfil de ${props.user}`}/>
+            <img src={`./assets/img/${props.image}`} alt={`Foto de perfil de ${props.user}`}/>
             <div class="text">
                 <strong>{props.user}</strong>
                 {props.name}
